64cbbdd3b4458c9d20f6b355f7521d33e9a5fb8f
Guilherme Canella <guycanella@gmail.com>
2026-01-17 14:29:44 -0300

    create parser

---
+++ crates/parser/src/ast.rs
@@ -0,0 +1,74 @@
+// crates/parser/src/ast.rs
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Literal {
+    Int(i64),
+    Float(f64),
+    String(String),
+    Bool(bool),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Eq,
+    NotEq,
+    Lt,
+    Gt,
+    LtEq,
+    GtEq,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Literal(Literal),
+
+    Identifier(String),
+
+    Binary {
+        op: BinaryOp,
+        lhs: Box<Expr>,
+        rhs: Box<Expr>,
+    },
+
+    Array(Vec<Expr>),
+
+    Index {
+        array: Box<Expr>,
+        index: Box<Expr>,
+    },
+    Call {
+        func: Box<Expr>,
+        args: Vec<Expr>,
+    },
+
+    Match {
+        value: Box<Expr>,
+        cases: Vec<(Expr, Expr)>,
+    },
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Stmt {
+    VariableDecl {
+        name: String,
+        value: Expr,
+        is_const: bool,
+    },
+
+    Assignment {
+        target: String,
+        value: Expr,
+    },
+
+    Expr(Expr),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Program {
+    pub statements: Vec<Stmt>,
+}
