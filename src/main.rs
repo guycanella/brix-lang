a46c0c4115209bcdc5fdb091cbaee33c15d24e0e
Guilherme Canella <guycanella@gmail.com>
2026-01-17 14:29:00 -0300

    update main for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use lexer::token::Token;
+use logos::Logos;
+
 fn main() {
-    println!("Hello, world!");
+    let code = r#"
+        const PI := 3.1415
+        var raio := 10
+        
+        // Testando array e calculo
+        dados := [1, 2, 3] --
+        area := PI * (raio * raio)
+    "#;
+
+    println!("--- Lendo código Brix ---");
+    println!("Código fonte:\n{}\n", code);
+    println!("--- Tokens Gerados ---");
+
+    let lexer = Token::lexer(code);
+
+    for result in lexer {
+        match result {
+            Ok(token) => println!("Token: {:?}", token),
+            Err(_) => println!("ERRO: Caractere inválido encontrado!"),
+        }
+    }
 }
